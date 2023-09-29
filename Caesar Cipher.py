254f78db6f0975e4623da28d66d7b51a0f660746
marlupatrocinio <marlupatrocinio01@gmail.com>
2023-09-29 14:54:30 -0300

    day 8

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,11 +1,10 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
 def encrypt(plainText, shiftAmount):
     codedText = ""
@@ -16,4 +15,16 @@ def encrypt(plainText, shiftAmount):
         codedText += newLetter
     print(f"The encoded text is {codedText}")
 
-encrypt(text, shift)
+def decrypt(plainText, shiftAmount):
+    decodedText = ""
+    for letter in plainText:
+        position = alphabet.index(letter)
+        newPosition = position - shiftAmount
+        newLetter = alphabet[newPosition]
+        codedText -= newLetter
+    print(f"The encoded text is {decodedText}")
+    
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
