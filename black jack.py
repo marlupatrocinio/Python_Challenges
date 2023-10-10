c5ca76203f2dcedaf777808f308b1d9799c30e64
Marlu Patrocínio <107137108+marlupatrocinio@users.noreply.github.com>
2023-10-10 11:42:27 -0300

    Update black jack.py

--- black jack.py
+++ black jack.py
@@ -1,25 +1,25 @@
 import random
 
 def dealCard():
-  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-  card = random.choice(cards)
-  return card
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
 def checkScore(userScore, cpuScore):
 
-  if userScore == 21:
-    print("User blackJack, you win!")
-    return True
-  elif cpuScore == 21:
-    print("Cpu blackJack, you lose!")
-    return True
-  elif userScore > 21:
-    print("You lose!")
-    return True
-  elif cpuScore > 21:
-    print("You win!")
-    return True
-  return False
+    if userScore == 21:
+        print("User blackJack, you win!")
+        return True
+    elif cpuScore == 21:
+        print("Cpu blackJack, you lose!")
+        return True
+    elif userScore > 21:
+        print("You lose!")
+        return True
+    elif cpuScore > 21:
+        print("You win!")
+        return True
+    return False
 
 user = []
 cpu = []
@@ -32,18 +32,24 @@ userScore = sum(user)
 cpuScore = sum(cpu)
 
 print(user, cpu)
-print(userScore, cpuScore)
+print("User Score:", userScore)
 endGame = False
 
 while not endGame:
-  option = input("Do you want to get another card? type yes or no: ")
-  if option == "yes":
-    user.append(dealCard())
-    userScore = sum(user)
+    option = input("Do you want to get another card? type yes or no: ")
+    if option == "yes":
+        user.append(dealCard())
+        userScore = sum(user)
+        print("User drew a card:", user[-1])
+    elif option == "no":
+        if not endGame:
+            while cpuScore < 17:
+                cpu.append(dealCard())
+                cpuScore = sum(cpu)
+                print("CPU drew a card:", cpu[-1])
 
-  endGame = checkScore(userScore, cpuScore)
-  print(user)
-  print(userScore)
-
-print(user, cpu)
-print(userScore, cpuScore)
+    endGame = checkScore(userScore, cpuScore)
+    print(user)
+    print("User Score:", userScore)
+    print(cpu)
+    print("CPU Score:", cpuScore)
